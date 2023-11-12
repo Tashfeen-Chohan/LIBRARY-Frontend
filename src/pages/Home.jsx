796c6324f3438b9923e083dc6cdad1414bcc96d8
Tashfeen Chohan <chohantashfeen@gmail.com>
2023-11-13 00:03:24 +0500

    refactord Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Pagination from "../components/Pagination";
 import Header from "../components/Header";
 import Table from "../components/Table";
+import { useCookies } from "react-cookie"
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Home = () => {
+
+  const navigate = useNavigate();
+  const [cookies, removeCookie] = useCookies([]);
+
+  useEffect(() => {
+    async function verifyCookie(){
+      // IF TOKEN NOT FOUND
+      if (!cookies.token){
+        navigate("/user-login")
+      }
+
+      try {
+        const res = await axios.post("http://localhost:3000/", {}, {withCredentials: true})
+
+        // IF STATUS == FALSE
+        if(!res.data.status){
+          removeCookie("token")
+          navigate("/user-login")
+        } 
+      } catch (error) {
+        console.log("Error verifying cookie: ", error)
+      }
+    }
+    verifyCookie()
+  }, [cookies, removeCookie, navigate ])
  
   return (
     <div className="flex justify-center items-center flex-col">
